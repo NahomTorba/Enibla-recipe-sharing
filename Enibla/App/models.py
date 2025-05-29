2c5dbdf67b758283468baf870553b3c8ea08f79d
Nahom <nahumtorba@gmail.com>
2025-05-29 20:22:08 +0300

    create a recipe model

--- Enibla/App/models.py
+++ Enibla/App/models.py
@@ -24,3 +24,26 @@ class UserProfile(models.Model):
     class meta:
         verbose_name = 'User Profile'
         verbose_name_plural = 'User Profiles'
+
+class Recipe(models.Model):
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name='recipes')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    TAG_CHOICES = (('breakfast', 'Breakfast'),('lunch', 'Lunch'),('dinner', 'Dinner'),('dessert', 'Dessert'),('snack', 'Snack'),('fasting', 'Fasting'),)
+    tags = models.CharField (max_length=100, blank=True)
+    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    def get_tag_choices_list(self):
+        if self.tags:
+            return self.tags.split(',')
+        return[]
+    class meta:
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
