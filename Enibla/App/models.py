b8313f40fbc0d6c2af03cb4c4d33494822fb1874
dagiyonas <dagmawi22@icloud.com>
2025-06-18 19:39:39 +0300

    fix: update models to use TextField for tags and fix Meta class

--- Enibla/App/models.py
+++ Enibla/App/models.py
@@ -10,9 +10,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True, null=True)
     CUISINE_CHOICES = [ ('Ethiopian', 'Ethiopian'), ('Eritrea', 'Eritrea'), ('African', 'African'), ('Italian', 'Italian'),('Mexican', 'Mexican'),('Chinese', 'Chinese'),('Japanese', 'Japanese'),('Indian', 'Indian'),('French', 'French'),('American', 'American'),('Korean', 'Korean'),('Spanish', 'Spanish'),('Middle Eastern', 'Middle Eastern'),('Brazilian', 'Brazilian'),('British', 'British')]
-    favorite_cuisines = models.CharField(blank=True)
+    favorite_cuisines = models.TextField(blank=True)
     profile_image = models.ImageField(upload_to='profile_pictures/', blank=True, null=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
-    created_at = models.DateTimeField(default= timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -21,9 +21,9 @@ class UserProfile(models.Model):
     def get_favorite_cuisines_list(self):
         if self.favorite_cuisines:
             return self.favorite_cuisines.split(',')
-        return[]
+        return []
     
-    class meta:
+    class Meta:
         verbose_name = 'User Profile'
         verbose_name_plural = 'User Profiles'
 
@@ -35,7 +35,7 @@ class Recipe(models.Model):
     ingredients = models.TextField()
     instructions = models.TextField()
     TAG_CHOICES = (('breakfast', 'Breakfast'),('lunch', 'Lunch'),('dinner', 'Dinner'),('dessert', 'Dessert'),('snack', 'Snack'),('fasting', 'Fasting'),)
-    tags = models.CharField (max_length=100, blank=True)
+    tags = models.TextField(blank=True)
     image = models.ImageField(upload_to='recipe_images/', blank=True, null=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
