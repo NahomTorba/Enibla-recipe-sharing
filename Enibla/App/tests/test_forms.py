afc1d54b59b9530f34dd421ec2d9b8f3180a48f0
Sofoniyas Dereje <princose16@gmail.com>
2025-07-29 12:44:30 +0300

    Structured unit tests

---
+++ Enibla/App/tests/test_forms.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from App.models import UserProfile
+from App.forms import RecipeForm
+
+
+class RecipeFormTests(TestCase):
+    def setUp(self):
+        # Create a user and associated profile to be the recipe author.
+        self.user = User.objects.create_user(username='testchef', password='strongpass')
+        self.profile = UserProfile.objects.create(user=self.user)
+
+    def test_valid_recipe_submission(self):
+        """Ensure that the recipe form is valid for correct data."""
+        form_data = {
+            'author': self.profile.id,
+            'title': 'Chocolate Cake',
+            'description': 'Delicious and rich chocolate cake',
+            'ingredients': 'Flour, eggs, chocolate, sugar, butter',
+            'instructions': 'Mix ingredients and bake.',
+            'tags': 'dessert, snack'
+        }
+        form = RecipeForm(data=form_data)
+        self.assertTrue(form.is_valid(), form.errors)
+
+    def test_invalid_recipe_submission_empty_fields(self):
+        form_data = {
+            'author': self.profile.id,
+            'title': '',
+            'description': '',
+            'ingredients': '',
+            'instructions': '',
+            'tags': ''  
+        }
+        form = RecipeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors)
+        self.assertIn('description', form.errors)
+        self.assertIn('ingredients', form.errors)
+        self.assertIn('instructions', form.errors)
