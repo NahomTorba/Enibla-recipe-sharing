8b4f6ee62661d83ad2bc5c09d056b4d7a5c7f09a
NahomTorba <nahumtorba@gmail.com>
2025-09-16 12:47:43 +0300

    refactor directory structure

--- Enibla/settings.py
+++ Enibla/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'App.apps.AppConfigClass'
+    'App.apps.AppConfigClass',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
