96e965615a0b720e5b238f1d7ca98709b45de6f6
NahomTorba <nahumtorba@gmail.com>
2025-09-24 18:10:15 +0300

    create forms.py file in reviewApp and create reviewForm class

--- App/forms.py
+++ App/forms.py
@@ -1,21 +1,3 @@
 from django import forms
 from App.models import Review
 
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = ['rating', 'comment']
-        widgets = {
-            'rating': forms.HiddenInput(),
-            'comment': forms.Textarea(attrs={
-                'rows': 4,
-                'placeholder': 'Share your thoughts about this recipe...',
-                'class': 'form-control'
-            })
-        }
-    
-    def clean_rating(self):
-        rating = self.cleaned_data.get('rating')
-        if not rating or rating < 1 or rating > 5:
-            raise forms.ValidationError('Please select a rating between 1 and 5 stars.')
-        return rating
--- reviewApp/models.py
+++ reviewApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
-from App.models import Recipe
+from recipeApp.models import Recipe
 
 
 # Create your models here.
