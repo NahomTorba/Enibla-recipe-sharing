d458f9ed401fe646627ad5be7566a68d47ce042d
NahomTorba <nahumtorba@gmail.com>
2025-09-25 22:20:10 +0300

    create serializer for models in userApp

--- userApp/serializer.py
+++ userApp/serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Recipe, Review, SavedRecipe
+from userApp.models import UserProfile
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
         fields = '__all__'
