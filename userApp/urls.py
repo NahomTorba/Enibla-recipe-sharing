87064060d36b41b2cc62c9cf6b297046c3a19a54
NahomTorba <nahumtorba@gmail.com>
2025-09-29 19:12:24 +0300

    write a rest viewsets and urls for recipeApp

--- userApp/urls.py
+++ userApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from . import views
+from userApp import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
