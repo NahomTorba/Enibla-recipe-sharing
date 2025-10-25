554dde83c0f47eda2658b688b6ce5374ee2b7e7c
NahomTorba <nahumtorba@gmail.com>
2025-10-25 22:44:00 +0300

    write test cases for SavedRecipe model

--- reviewApp/tests/test_models.py
+++ reviewApp/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from reviewApp.models import Review, Recipe
+from reviewApp.models import Review, SavedRecipe
+from recipeApp.models import Recipe
 
 class ReviewModelTest(TestCase):
     
@@ -76,3 +77,35 @@ class ReviewModelTest(TestCase):
         reviews = Review.objects.all()
         self.assertEqual(reviews[0], review1)
         self.assertEqual(reviews[1], review2)
+
+class SavedRecipeModelTest(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="password")
+        self.recipe = Recipe.objects.create(title="Test Recipe", description="Test Description")
+
+    def test_create_valid_saved_recipe(self):
+        saved_recipe = SavedRecipe.objects.create(
+            user=self.user,
+            recipe=self.recipe
+        )
+        self.assertEqual(saved_recipe.user, self.user)
+        self.assertEqual(saved_recipe.recipe, self.recipe)
+        self.assertTrue(saved_recipe.saved_at)
+
+    def test_unique_user_recipe_constraint(self):
+        # Save the recipe for the user
+        SavedRecipe.objects.create(user=self.user, recipe=self.recipe)
+
+        # Try to save the same recipe for the same user again
+        with self.assertRaises(Exception):
+            SavedRecipe.objects.create(user=self.user, recipe=self.recipe)
+
+    def test_saved_recipe_ordering(self):
+        saved_recipe1 = SavedRecipe.objects.create(user=self.user, recipe=self.recipe)
+        saved_recipe2 = SavedRecipe.objects.create(user=self.user, recipe=self.recipe)
+
+        # Check if saved recipes are ordered by saved_at in descending order
+        saved_recipes = SavedRecipe.objects.all()
+        self.assertEqual(saved_recipes[0], saved_recipe2)
+        self.assertEqual(saved_recipes[1], saved_recipe1)
